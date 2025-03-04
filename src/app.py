ce758e0a757dfe2ed59a1f76f7b34c0b112c6b40
Dyimah Ansah <hdyimah@yahoo.com>
2025-03-04 15:43:39 +0000

    added features back to model

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import power_transform, MinMaxScaler, OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-# your code here
+raw_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/linear-regression-project-tutorial/main/medical_insurance_cost.csv")
+
+model_data = raw_data[['age', 'bmi', 'smoker', 'children', 'region', 'charges']]
